efc63a6db162f73b29d47f2ae3243f7a8173093c
juchanpark <jpark@all-inbox.com>
2018-01-03 00:20:26 -0800

    UPDATE: cell dropdown and project dropdown both change viewport

--- src/ViewPort.js
+++ src/ViewPort.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {helpers,getProjects, getCells, getValue} from "./flux/helper";
-//This did not work
-//import FluxViewport from 'flux-viewport';
+var _ = require('lodash');
 
 class ViewPort extends Component {
     constructor(props) {
@@ -30,6 +29,7 @@ class ViewPort extends Component {
 
             getCells(firstProject).then(function (data) {
                 var firstCell = data.entities[0];
+                console.log(data.entities);
                 self.setState({
                     cells:data.entities,
                     cell: firstCell
@@ -44,8 +44,7 @@ class ViewPort extends Component {
 
     }
     initViewPort() {
-        // Tried everything under the sun to get this section working but no dice¯\_(ツ)_/¯
-                // attach the viewport to the #div view
+                // attach the viewport to the #div view thanks George
                 this.viewport = new window.FluxViewport(document.querySelector("#view"));
                 // set up default lighting for the viewport
                 this.viewport.setupDefaultLighting();
@@ -64,8 +63,39 @@ class ViewPort extends Component {
     }
 
     handleChange (event) {
-        this.setState({project: event.target.value});
-        console.log(this.state.project);
+        var self = this;
+
+        switch(event.target.name) {
+            case 'projectSelect':
+                var changedProj = _.find(self.state.projects, {id: event.target.value});
+                this.setState({project: changedProj});
+                getCells(changedProj).then(function (data) {
+
+                    var firstCell = data.entities[0];
+                    self.setState({
+                        cells:data.entities,
+                        cell: firstCell
+                    });
+                    getValue(changedProj, firstCell).then(function(data) {
+                        self.renderViewport(data);
+                    });
+
+                });
+                break;
+            case 'cellSelect':
+                var changedCell = _.find(self.state.cells, {id: event.target.value});
+                self.setState({cell: changedCell});
+                getValue(self.state.project, changedCell).then(function(data) {
+                    self.renderViewport(data);
+                });
+                break;
+            default:
+                getValue(self.state.project, self.state.cell).then(function(data) {
+                    self.renderViewport(data);
+                });
+                break;
+        }
+
     }
     handleClick (){
         //handle logout
@@ -79,12 +109,6 @@ class ViewPort extends Component {
                 <header className='App-header'>
                     <h1 className='App-title'>Flux Seed Project</h1>
                     <div id='actions'>
-                        <div className='select'><select className='project' onChange={this.handleChange.bind(this)}>{
-                            this.state.projects.length ? this.state.projects.map(function(project, i){
-                               return <option value={project.id} key={i}> {project.name}</option>
-                            }) : <option>No Data</option>
-                        }
-                        </select></div>
                         <div id='logout' onClick={this.handleClick}>logout</div>
                     </div>
                 </header>
@@ -92,10 +116,16 @@ class ViewPort extends Component {
                      <div id='geometry'>
                         <div id='view'></div>
                     </div>
-                    <div className='select' id="cellSelect"><select className='cell'>
+                    <div className='select' id="projectSelect"><select className='project' name='projectSelect' onChange={this.handleChange.bind(this)}>{
+                        this.state.projects.length ? this.state.projects.map(function(project, i){
+                            return <option value={project.id} key={i}> {project.name}</option>
+                        }) : <option>No Data</option>
+                    }
+                    </select></div>
+                    <div className='select' id="cellSelect"><select className='cell' name='cellSelect' onChange={this.handleChange.bind(this)}>
                         {
                             this.state.cells.length ? this.state.cells.map(function(cell, i) {
-                                return <option value={cell.id} key={i}>cell.label</option>
+                                return <option value={cell.id} key={i}>{cell.label}</option>
                             }) : <option>No Data</option>
                         }
                     </select></div>
